d646f48463988c5bdf6c502e3ddc4006fe941b1c
harshs14 <harsh.singh1411@gmail.com>
2018-11-26 15:40:10 +0530

    initiating jwt

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from snippets import views
 from rest_framework.schemas import get_schema_view
+from rest_framework_jwt.views import obtain_jwt_token
+
 
 schema_view = get_schema_view(title='Pastebin API')
 
@@ -14,4 +16,5 @@ router.register(r'users', views.UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('schema/', schema_view),
+    re_path(r'^api-token-auth/', obtain_jwt_token),
 ]
